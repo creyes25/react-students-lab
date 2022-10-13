0e2d36defb2f0542957002c931fcae94b0f33cb4
creyes25 <carolina.reyes2022@gmail.com>
2022-10-12 20:19:32 -0500

    add a score component and render it

---
+++ src/Score.jsx
@@ -0,0 +1,9 @@
+const Score = (props) => {
+  return (
+    <>
+      <h5>On {props.scores.date} had a score of {props.scores.score}</h5>
+    </>
+  )
+}
+
+export default Score
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,12 +1,16 @@
 import { students } from "./data"
+import Score from "./Score"
 
 const Student = (props) => {
   return (
     <div>
       <h1>Student Name: {props.student.name}</h1>
       <h3>Bio: <p>{props.student.bio}</p></h3>
+      {props.student.scores.map(score => 
+        <Score key={props.student.name} scores={score}/>
+      )}
     </div>
   );
 }
- 
+
 export default Student;
